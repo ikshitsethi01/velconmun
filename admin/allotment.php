f4a0ee752c723fc7bafcf5825363253f8a8ba9dc
Ikshit Sethi <94429791+ikshitsethi01@users.noreply.github.com>
2026-02-15 18:26:45 +0530

    Create allotment.php

---
+++ admin/allotment.php
@@ -0,0 +1,6 @@
+<form method="POST" action="api/add-job.php">
+    <input type="hidden" name="key" value="SUPER_SECRET_KEY">
+    <label>Committee Code</label>
+    <input type="text" name="committeeCode" required>
+    <button type="submit">Run Allotment</button>
+</form>
