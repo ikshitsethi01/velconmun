ef439c4ba6376007e7c7308fa6a4049004a42b74
Ikshit Sethi <94429791+ikshitsethi01@users.noreply.github.com>
2026-02-15 18:29:51 +0530

    Update allotment.php

--- admin/allotment.php
+++ admin/allotment.php
@@ -1,6 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Allotment Admin</title>
+</head>
+<body>
+
+<h2>Run Allotment</h2>
+
 <form method="POST" action="api/add-job.php">
+    <label>Committee Code:</label><br>
+    <input type="text" name="committeeCode" required><br><br>
+
     <input type="hidden" name="key" value="SUPER_SECRET_KEY">
-    <label>Committee Code</label>
-    <input type="text" name="committeeCode" required>
+
     <button type="submit">Run Allotment</button>
 </form>
+
+</body>
+</html>
